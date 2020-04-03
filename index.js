2e2bf35df3d14463d08c18d93ee2379ffb682db0
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-04-03 11:24:35 -0400

    Friyay question one answer

---
+++ index.js
@@ -0,0 +1,16 @@
+let i;
+
+function multiples(num) {
+  for (i = 1; i < num + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+multiples(30);
